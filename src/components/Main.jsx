7fc8e554f8a5370deaffa92b09488b014e6d7371
jtaff <jtaff@2u.com>
2025-02-07 10:13:12 -0500

    add backup for video media type

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,32 @@ import React from 'react'
 
 export default function Main(props) {
   const {data} = props
+  console.log(data.media_type);
+
   return (
-    <div className="imgContainer">
-        <img src={data.url} alt={data.title} className="bgImage" />
-    </div>
-      
-    
+    <>
+     {data.media_type =="image" ? (  
+      <div className="imgContainer">
+            <img src={data.url} alt={data.title} className="bgImage" />
+      </div>) : 
+      (
+      <div className="videoContainer">
+        <iframe width="960"  
+            src={data.url} 
+            title={data.title} 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" 
+            allowfullscreen="">
+        </iframe>
+      </div>
+     ) 
+
+
+     }
+    </>
+   
+
+
   )
 }
